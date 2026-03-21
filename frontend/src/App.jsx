0489b9bf9c8af51390d0410de6722ac172bf1334
Samarjeet <samarsabale1021@gmail.com>
2026-03-21 11:49:10 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created favicon.svg for the application
    - Added icons.svg containing various SVG icons
    - Implemented App.css for styling components
    - Created App.jsx as the main application component
    - Added hero.png as an asset for the application
    - Set up index.css to import Tailwind CSS
    - Created main.jsx to render the App component
    - Configured Vite with vite.config.js for React and Tailwind CSS integration.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-4xl text-red-600 font-bold'>Smart phishing detection and url analyser</h1>
+    </div>
+  )
+}
+
+export default App
