3b92f8101b7d94c1aa073db78b00310bb514e6c4
Samarjeet <samarsabale1021@gmail.com>
2026-04-16 10:14:19 +0530

    feat(frontend): implement routing with Home component and add react-router-dom, and updated requirement.txt  backend file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
-    <div>
-      <h1 className='text-4xl text-red-600 font-bold'>Smart phishing detection and url analyser</h1>
-    </div>
+    <BrowserRouter>
+
+    <Link to='/' >
+      <h2 className='text-xl font-semibold text-gray-900'>Home</h2>
+    </Link>
+
+      <Routes>
+        <Route path='/' element={<Home />} >Home</Route>
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
 
