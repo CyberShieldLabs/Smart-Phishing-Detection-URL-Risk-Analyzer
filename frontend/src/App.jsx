3fe2038448d6557819e82a09ac10b8b9b8c0075c
Samarjeet <samarsabale1021@gmail.com>
2026-05-07 11:52:36 +0530

    feat(frontend): implement routing with Analyzer and Error404 components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Error404 from './pages/Error404';
+import AppRouts from './routes/AppRouts';
 function App() {
   return (
-    <BrowserRouter>
-
-    <Link to='/' >
-      <h2 className='text-xl font-semibold text-gray-900'>Home</h2>
-    </Link>
-
-      <Routes>
-        <Route path='/' element={<Home />} >Home</Route>
-      </Routes>
-      
-    </BrowserRouter>
+    <Router>
+      <div className='w-full min-h-[100vh] bg-amber-100'>
+        <div>
+          <AppRouts />
+        </div>
+      </div>
+    </Router>
   )
 }
 
---
+++ frontend/src/pages/Error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error404() {
+  return (
+    <div>
+      <h2 className='text-4xl text-red-600 font-semibold'>404 :( Error Page not found!</h2>
+    </div>
+  )
+}
+
+export default Error404
